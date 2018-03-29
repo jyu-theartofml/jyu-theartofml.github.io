6ce5c9abfe328650eef342609688401c6876164d
Jenny Yu <jypucca@gmail.com>
2018-03-29 13:24:26 -0600

    Update chord.js

--- posts/chord.js
+++ posts/chord.js
@@ -22,7 +22,7 @@ var layout = d3.layout.chord()
 var path = d3.svg.chord()
 .radius(innerRadius);
 
-var svg = d3.select("div#chart").append("svg")
+var svg = d3.select("body").append("svg")
 .attr("width", (width+margin.left+margin.right))
 .attr("height", (height+margin.top+margin.bottom))
 .append("g")
@@ -35,7 +35,7 @@ svg.append("circle")
 d3.csv("cities2.csv", function(cities) {
 d3.json("location_matrix.json", function(matrix) {
 
-// Compute the chord layout as nxn matrix.
+// Compute the chord layout.
 layout.matrix(matrix);
 
 // Add a group per neighborhood.
@@ -43,26 +43,11 @@ var group = svg.selectAll(".group")
 .data(layout.groups)
 .enter().append("g")
 .attr("class", "group")
-.on("mouseover", mouseover);
+.on("mouseover", mouseover);;
 
-// Add a mouseover title. d.value refers the total values in that row
+// Add a mouseover title for each arc.
 group.append("title").text(function(d, i) {
  return Math.round(d.value) + "people leaving " +  cities[i].name;});
-
-group.append("svg:text")
- 	.each(function(d) { d.angle = ((d.startAngle + d.endAngle) / 2);})
- 	.attr("dy", ".25em")
- 	.attr("class", "titles")
- 	.attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
- 	.attr("transform", function(d,i) {
- 		var c = arc.centroid(d);
- 		return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
- 		+ "translate(" + (innerRadius + 15) + ")" //how close the labels are to the outer arc
- 		+ (d.angle > Math.PI ? "rotate(180)" : "")
- 	})
- 	.text(function(d,i) { return cities[i].name; });
-
-
 
 // Add the group arc.
 var groupPath = group.append("path")
@@ -70,40 +55,45 @@ var groupPath = group.append("path")
 .attr("d", arc)
 .style("fill", function(d, i) { return cities[i].color; });
 
-// Add a text label.
-var groupText = group.append("text")
-  .attr("x", 6)
-  .attr("dy", '0.35em');
-
-
-
-// Remove the labels that don't fit. :(
-//groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 8 < this.getComputedTextLength(); })
-//.remove();
+//add text label of the groups
+group.append("svg:text")
+  	.each(function(d) { d.angle = ((d.startAngle + d.endAngle) / 2);})
+  	.attr("dy", ".25em")
+  	.attr("class", "titles")
+  	.attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+  	.attr("transform", function(d,i) {
+  		var c = arc.centroid(d);
+  		return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+  		+ "translate(" + (innerRadius + 15) + ")" //how close the labels are to the outer arc
+  		+ (d.angle > Math.PI ? "rotate(180)" : "")
+  	})
+  	.text(function(d,i) { return cities[i].name; });
 
 // Add the chords.
 var chord = svg.selectAll(".chord")
-.data(layout.chords)
-.enter().append("path")
-.attr("class", "chord")
-.style("fill", function(d) { return cities[d.source.index].color; })
-.attr("d", path);
+    .data(layout.chords)
+		.enter().append("path")
+		.attr("class", "chord")
+		.style("fill", function(d) { return cities[d.source.index].color; })
+		.attr("d", path);
 
-// Add an elaborate mouseover title for each chord.
- chord.append("title").text(function(d) {
- return cities[d.source.index].name
- + " → " + cities[d.target.index].name
- + ": " + (d.source.value)
- + "\n" + cities[d.target.index].name
- + " → " + cities[d.source.index].name
- + ": " + (d.target.value);
- });
 
+// tool tip info for the chords
+chord.append("title").text(function(d) {
+		 return cities[d.source.index].name
+		 + " → " + cities[d.target.index].name
+		 + ": " + (d.source.value)
+		 + "\n" + cities[d.target.index].name
+		 + " → " + cities[d.source.index].name
+		 + ": " + (d.target.value);
+		 });
+//mouse over function
 function mouseover(d, i) {
-chord.classed("fade", function(p) {
-return p.source.index != i
-&& p.target.index != i;
-});
-}
+		chord.classed("fade", function(p) {
+		return p.source.index != i
+		&& p.target.index != i;
+		});
+		}
+
 });
 });
