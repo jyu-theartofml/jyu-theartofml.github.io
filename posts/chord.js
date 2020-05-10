7ffd9cb063fa9009ee9c36d8e1e23d090564654d
Jenny Yu <jenny.yu@kimetrica.com>
2020-05-09 20:00:59 -0600

    edit

--- posts/chord.js
+++ posts/chord.js
@@ -22,7 +22,8 @@ var layout = d3.layout.chord()
 var path = d3.svg.chord()
 .radius(innerRadius);
 
-var svg = d3.select("div#chart").append("svg")
+//var svg = d3.select("#chart").append("svg")
+var svg=d3.select("svg")
 .attr("width", (width+margin.left+margin.right))
 .attr("height", (height+margin.top+margin.bottom))
 .append("g")
